10f42772d20c75d9c4a6dd0168600d248f40c03a
Guerson <ultim8p@gmail.com>
2023-03-10 01:21:32 +0100

    Initial Commit

---
+++ Tests/NoAPITests/NoAPITests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import NoAPI
+
+final class NoAPITests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(NoAPI().text, "Hello, World!")
+    }
+}
